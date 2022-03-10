b0386b57675b37836473df10c97f7907a1f454e6
wanghao <372356818@qq.com>
2022-03-10 16:10:18 +0800

    首次提交

---
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -0,0 +1,139 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.service.AlphaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+@Controller
+@RequestMapping("/alpha")
+public class AlphaController {
+
+    @Autowired
+    private AlphaService alphaService;
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayhello(){
+        return "hello spring boot.";
+    }
+
+    @RequestMapping("/data")
+    @ResponseBody
+    public String getData(){
+        return alphaService.find();
+    }
+
+    @RequestMapping("/http")
+    public void http(HttpServletRequest request, HttpServletResponse response){
+        System.out.println(request.getMethod());// 获取请求数据
+        System.out.println(request.getServletPath());
+        Enumeration<String> enumeration = request.getHeaderNames();//请求的消息头信息
+        while(enumeration.hasMoreElements()){
+            String name = enumeration.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + ":" + value);
+        }
+        System.out.println(request.getParameter("code"));
+
+        //返回响应数据
+        response.setContentType("text/html;charset = utf-8");
+        try (
+                PrintWriter writer =  response.getWriter();
+                ){
+            writer.write("<h1> my first website <h1>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    //Get请求(浏览器向服务器获取数据）
+    // 在GET请求下传递参数的两种方式
+    // 第一种/ student ? current = 1 & limit = 20;
+    @RequestMapping(path = "/student",method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudent(
+            @RequestParam(name = "current",required = false, defaultValue = "1") int current,
+            @RequestParam(name = "limit",required = false,defaultValue = "10") int limit){
+        System.out.println(current);
+        System.out.println(limit);
+        return "the student";
+    }
+
+    // 第二种/thestudent/123
+    @RequestMapping(path = "/thestudent/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public String getthestudent(@PathVariable("id") int id){
+        System.out.println(id);
+        return "the student";
+    }
+
+
+    // POST请求（浏览器向服务器提交数据）
+    @RequestMapping(path = "/student",method = RequestMethod.POST)
+    @ResponseBody
+    public String saveStudent(String name,int age){
+        System.out.println(name);
+        System.out.println(age);
+        return "success";
+    }
+
+    //向浏览器响应HTML数据 方法一
+    @RequestMapping(path = "/teacher",method = RequestMethod.GET)
+    public ModelAndView getTeacher(){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("name","李四");
+        mav.addObject("age",24);
+        mav.setViewName("demo/view");
+        return mav;
+    }
+    //方法二
+    @RequestMapping(path = "/school",method = RequestMethod.GET)
+    public String getSchool(Model model){
+        model.addAttribute("name","重庆邮电");
+        model.addAttribute("age",72);
+        return "demo/view";
+    }
+    //向浏览器响应JSON数据（异步请求）Java对象 ->JSON字符串->JS对象;
+    @RequestMapping(path = "/emp",method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String,Object> getEmp(){
+        Map<String,Object> emp = new HashMap<>();
+        emp.put("name","张三");
+        emp.put("age",25);
+        emp.put("salary",12000.00);
+        return emp;
+    }
+    @RequestMapping(path = "/emps",method = RequestMethod.GET)
+    @ResponseBody
+    public List<Map<String,Object>> getEmps(){
+        List<Map<String,Object>> list = new ArrayList<>();
+        Map<String,Object> emp = new HashMap<>();
+        emp.put("name","张三");
+        emp.put("age",25);
+        emp.put("salary",12000.00);
+        list.add(emp);
+
+        emp.put("name","李四");
+        emp.put("age",24);
+        emp.put("salary",10000.00);
+        list.add(emp);
+
+        emp.put("name","王五");
+        emp.put("age",26);
+        emp.put("salary",14000.00);
+        list.add(emp);
+        return list;
+    }
+
+}
